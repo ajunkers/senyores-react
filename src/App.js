9458b6c8d3fa0fc4a0660c5db9a1d38400dac202
Andrea Vidal <11.andrea.v.p@gmail.com>
2021-03-08 20:24:12 +0100

    Señores que te apuntan con el dedo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  const senyores = [
+    {
+      id: 1,
+      nombre: "El Fary",
+      profesion: "Influencer",
+      foto: "fary.jpg",
+      estado: "R.I.P.",
+      twitter: "Pendiente",
+      marcado: true
+    },
+    {
+      id: 2,
+      nombre: "Julio Iglesias",
+      profesion: "Youtuber",
+      foto: "julio.jpg",
+      estado: "Vivo",
+      twitter: "@JulyChurchs",
+      marcado: false
+    },
+    {
+      id: 3,
+      nombre: "Bertín Osborne",
+      profesion: "Java Developer",
+      foto: "bertin.jpg",
+      estado: "Vivo",
+      twitter: "@BertinOsborne",
+      marcado: true
+    }
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="cabecera">
+        <h1>Señores que te apuntan con el dedo</h1>
+
+        <p className="totales"><span className="nmarcados">0</span> señores que te apuntan con el dedo marcados</p>
+
+        <a className="accion-marcar" href="marcar-todos">Marcar todos</a>
       </header>
-    </div>
+      <main>
+
+        {senyores.map(senyor =>
+
+          <article className="senyor">
+            <div className="avatar">
+              <img src={`img/${senyor.foto}`} alt={`${senyor.nombre} señalándote con el dedo`} />
+              <span className="inicial">{senyor.foto.split("")[0].toLocaleUpperCase()}</span>
+            </div>
+            <div className="info">
+              <h2>{senyor.nombre}</h2>
+              <ul>
+                <li><span className="etiqueta">Profesión:</span>{senyor.profesion}</li>
+                <li><span className="etiqueta">Estado:</span> {senyor.estado}</li>
+                <li><span className="etiqueta">Twitter:</span> {senyor.twitter}</li>
+              </ul>
+            </div>
+            <i className="icono fas fa-check"></i>
+          </article>)}
+      </main>
+    </>
   );
 }
 
